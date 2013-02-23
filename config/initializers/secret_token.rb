30694cacce52ac9e3b25d20d69e19e7383ab44c6
Jonathan Goldberg <goldberg.jonathan@gmail.com>
2013-02-23 15:54:31 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GamesOfThronesBoardGame::Application.config.secret_token = '7d4715365179cf0437121c38af67304a77c1dd127d2d3dbb248ed2adbbad955d68004604a332941e0c4fb3dfa73b4fd988f9a4f1cfe2fdbc818ed0ac86aa8685'
